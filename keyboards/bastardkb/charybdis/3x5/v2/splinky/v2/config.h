zuesmajor/bastardkb-qmk

bastardkb: split splinky-based keyboards to distinguish between Splinky v2 and v3 pinout

keyboards/bastardkb/charybdis/3x5/v2/splinky/v2/config.h
@@ -18,28 +18,10 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROW_PINS \
-    { GP29, GP26, GP5, GP4, GP9 }
-
-/* Handedness. */
-#define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* serial.c configuration (for split keyboard). */
-#define SOFT_SERIAL_PIN GP1
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
+#define MATRIX_COL_PINS \
+    { GP28, GP15, GP6, GP7, GP8 }
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID0
-#define SPI_MISO_PIN GP20
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define SPI_SCK_PIN GP18
+#define SPI_MOSI_PIN GP19
+#define PMW33XX_CS_PIN GP14

keyboards/bastardkb/charybdis/3x5/v2/splinky/v3/config.h
@@ -18,28 +18,10 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROW_PINS \
-    { GP29, GP26, GP5, GP4, GP9 }
-
-/* Handedness. */
-#define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* serial.c configuration (for split keyboard). */
-#define SOFT_SERIAL_PIN GP1
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
+#define MATRIX_COL_PINS \
+    { GP28, GP21, GP6, GP7, GP8 }
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID0
-#define SPI_MISO_PIN GP20
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define SPI_SCK_PIN GP22
+#define SPI_MOSI_PIN GP23
+#define PMW33XX_CS_PIN GP16

keyboards/bastardkb/charybdis/3x6/v2/splinky/v2/config.h
@@ -18,28 +18,10 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROW_PINS \
-    { GP29, GP26, GP5, GP4, GP9 }
-
-/* Handedness. */
-#define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* serial.c configuration (for split keyboard). */
-#define SOFT_SERIAL_PIN GP1
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
+#define MATRIX_COL_PINS \
+    { GP27, GP28, GP15, GP6, GP7, GP8 }
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID0
-#define SPI_MISO_PIN GP20
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define SPI_SCK_PIN GP18
+#define SPI_MOSI_PIN GP19
+#define PMW33XX_CS_PIN GP14

keyboards/bastardkb/charybdis/3x6/v2/splinky/v3/config.h
@@ -18,28 +18,10 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROW_PINS \
-    { GP29, GP26, GP5, GP4, GP9 }
-
-/* Handedness. */
-#define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* serial.c configuration (for split keyboard). */
-#define SOFT_SERIAL_PIN GP1
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
+#define MATRIX_COL_PINS \
+    { GP27, GP28, GP21, GP6, GP7, GP8 }
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID0
-#define SPI_MISO_PIN GP20
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define SPI_SCK_PIN GP22
+#define SPI_MOSI_PIN GP23
+#define PMW33XX_CS_PIN GP16

stawil15/qmk_firmware

keyboards/bastardkb/charybdis/3x5/v2/splinky/v2/config.h
@@ -18,28 +18,10 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROW_PINS \
-    { GP29, GP26, GP5, GP4, GP9 }
-
-/* Handedness. */
-#define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* serial.c configuration (for split keyboard). */
-#define SOFT_SERIAL_PIN GP1
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
+#define MATRIX_COL_PINS \
+    { GP28, GP15, GP6, GP7, GP8 }
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID0
-#define SPI_MISO_PIN GP20
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define SPI_SCK_PIN GP18
+#define SPI_MOSI_PIN GP19
+#define POINTING_DEVICE_CS_PIN GP14

keyboards/bastardkb/charybdis/3x5/v2/splinky/v3/config.h
@@ -18,28 +18,10 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROW_PINS \
-    { GP29, GP26, GP5, GP4, GP9 }
-
-/* Handedness. */
-#define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* serial.c configuration (for split keyboard). */
-#define SOFT_SERIAL_PIN GP1
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
+#define MATRIX_COL_PINS \
+    { GP28, GP21, GP6, GP7, GP8 }
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID0
-#define SPI_MISO_PIN GP20
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define SPI_SCK_PIN GP22
+#define SPI_MOSI_PIN GP23
+#define POINTING_DEVICE_CS_PIN GP16

keyboards/bastardkb/charybdis/3x6/v2/splinky/v2/config.h
@@ -18,28 +18,10 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROW_PINS \
-    { GP29, GP26, GP5, GP4, GP9 }
-
-/* Handedness. */
-#define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* serial.c configuration (for split keyboard). */
-#define SOFT_SERIAL_PIN GP1
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
+#define MATRIX_COL_PINS \
+    { GP27, GP28, GP15, GP6, GP7, GP8 }
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID0
-#define SPI_MISO_PIN GP20
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define SPI_SCK_PIN GP18
+#define SPI_MOSI_PIN GP19
+#define POINTING_DEVICE_CS_PIN GP14

keyboards/bastardkb/charybdis/3x6/v2/splinky/v3/config.h
@@ -18,28 +18,10 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROW_PINS \
-    { GP29, GP26, GP5, GP4, GP9 }
-
-/* Handedness. */
-#define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* serial.c configuration (for split keyboard). */
-#define SOFT_SERIAL_PIN GP1
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
+#define MATRIX_COL_PINS \
+    { GP27, GP28, GP21, GP6, GP7, GP8 }
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID0
-#define SPI_MISO_PIN GP20
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define SPI_SCK_PIN GP22
+#define SPI_MOSI_PIN GP23
+#define POINTING_DEVICE_CS_PIN GP16